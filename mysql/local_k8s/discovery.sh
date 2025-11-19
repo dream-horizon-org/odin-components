ac0c4c60be51e650fcf9dd2b26ba4d8dd5143a7e
sharad-vishe <sharad.vishe@dream11.com>
2025-11-19 16:12:49 +0530

    updated discovery

--- mysql/local_k8s/discovery.sh
+++ mysql/local_k8s/discovery.sh
@@ -15,7 +15,7 @@ else
 fi 
 
 function get_endpoints() {
-  kubectl get endpoints -n ${NAMESPACE} -l app.kubernetes.io/instance=${RELEASE_NAME},app.kubernetes.io/component=$1 | grep headless | awk '{gsub(":[0-9]+", "", $2); print $2}'
+  kubectl get endpointslices -n ${NAMESPACE} -l app.kubernetes.io/instance=${RELEASE_NAME},app.kubernetes.io/component=$1 | grep headless | awk '{gsub(":[0-9]+", "", $4); print $4}'
 }
 
 WRITER_ENDPOINT=$(get_endpoints primary)
